b1b83f5a711cb246d515f8d94d58247fa41caec5
André Fortini <124889918+andrefrtn@users.noreply.github.com>
2026-05-03 18:31:36 -0300

    feat(produto): adiciona enum de categorias de produtos

---
+++ src/back/model/CategoriaProduto.java
@@ -0,0 +1,12 @@
+package com.jardimmagnolia.model;
+
+public enum CategoriaProduto {
+    BUQUES,
+    ORQUIDEAS,
+    ANIVERSARIO,
+    ROSAS,
+    CAMPO,
+    PRESENTES,
+    CESTAS,
+    PLANTAS
+}
