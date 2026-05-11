69b3639a3dbed534db430b4776e5463229eeb2e0
Júlia Fonseca Lasmar <164426900+julialasmar@users.noreply.github.com>
2026-05-11 11:30:23 -0300

    Create PagamentoRepository

---
+++ src/backend/src/main/java/com/jardimmagnolia/repository/PagamentoRepository.java
@@ -0,0 +1,14 @@
+package com.jardimmagnolia.repository;
+
+import com.jardimmagnolia.model.Pagamento; 
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+    Optional<Pagamento> findByPedidoId(Long pedidoId);
+    List<Pagamento> findAllByOrderByCriadoEmDesc();
+}
